bb4dfcc85867f7b05585a4b06797a53c052fcfe4
RuzleBG <kiril.lazarov@mail.bg>
2022-01-31 13:10:48 +0200

    i want to eat

--- routes/articles.js
+++ routes/articles.js
@@ -1,14 +1,30 @@
 const express=require('express');
 const router=express.Router();
+const Article=require('../models/article')
 
 router.get('/',(req,res)=>{
+    res.send('we made it');
+});
+router.get('/:id',(req,res)=>{
     res.send('we made it');
 });
 router.get('/new',(req,res)=>{
     res.render("articles/new.ejs");
 });
-router.post('/',(req,res)=>{
-    
-})
+router.post('/',async (req,res)=>{
+    let article=new Article({
+    title: req.body.title,
+    description: req.body.description,
+    markdown: req.body.markdown                
+    });
+try{
+    new_article=await article.save();
+    res.redirect(`/articles/${article.id}`);
+}
+    catch(e){
+        console.log(e);
+        res.render("articles/new", {article:article})
+    }
+});
 
 module.exports=router;
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+const { urlencoded } = require('express');
 const express=require('express');
 const mongoose=require('mongoose');
 const articleRouter=require('./routes/articles');
+
+mongoose.connect("mongodb://localhost/blog");
+
 const app=express();
 
 app.set('view engine', 'ejs');
-
-app.use('/articles',articleRouter);
+app.use(express.urlencoded({
+    extended:false
+}))
 
 app.get('/',(req,res)=>{
     const articles=[{
@@ -15,6 +20,8 @@ app.get('/',(req,res)=>{
     }];
     res.render('index',{articles:articles});
 });
+
+app.use('/articles',articleRouter);
 
 app.listen(5000,()=>{
     console.log("Server Started on Port 5000")
